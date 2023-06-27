d5404d37a7ada2722efc48192909c71a1de67cb5
JJ Jiang <jingjingjiang2017@gmail.com>
2023-06-27 19:25:53 +0800

    init

--- src/tools.py
+++ src/tools.py
@@ -1,1 +1,30 @@
+# @File :tools.py
+# @Time :2022/6/22
+# @Desc :
+import re
+import json
+import os
+
+
+# *********************************************************************
+def read_txt(file):
+    with open(file, "r") as f:
+        data = [line.strip('\n') for line in f.readlines()]
+    return data
+
+
+def write_txt(file, s):
+    with open(file, 'a+') as f:
+        f.write(s)
+
+
+def save_json(data, file_path):
+    with open(file_path, "w") as f:
+        json.dump(data, f, sort_keys=True, indent=4)
+
+
+def load_json(file_path):
+    with open(file_path, "r") as f:
+        return json.load(f)
+
 
