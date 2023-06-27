94abe9922bee3decb580c5df63705e54a52ba6a8
JJ Jiang <jingjingjiang2017@gmail.com>
2023-06-27 19:21:17 +0800

    init

---
+++ scripts/okvqa_vlt5_mixphm.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+# *********************************************************
+data_name=okvqa
+
+r=12
+e=4
+rank=8
+n=4
+w=0.2
+
+for k in 16 32 64 100 500 1000;do
+  name=epoch1000_bs16_lr5e-3_k${k}_r${r}_rank${rank}_n${n}_expert${e}+Lra_w${w}_D1D2SAdn
+  output=Exp-VLT5-MixPHM/${data_name}/$name
+  mkdir -p $output
+  cp $0 $output/run.sh
+  CUDA_VISIBLE_DEVICES=$1 PYTHONPATH=$PYTHONPATH:./src \
+    torchrun --nproc_per_node=$2 \
+    src/vl_t5/train.py \
+    --distributed \
+    --data_name $data_name \
+    --test val \
+    --k $k \
+    --seeds "13, 21, 42, 87, 100" \
+    --apply_phm_moe True \
+    --hypercomplex_adapter True \
+    --hypercomplex_division $n \
+    --learn_phm True \
+    --shared_phm_rule True \
+    --factorized_phm True \
+    --phm_rank $rank \
+    --reduction_factor $r \
+    --num_experts $e \
+    --inference_level 3 \
+    --sharing_down 1 \
+    --sharing_up 0 \
+    --use_adapter_r2loss True \
+    --using_mi_loss True \
+    --red_w $w \
+    --align_w $w \
+    --use_adapter_cross_attn False \
+    --use_adapter_self_attn False \
+    --lr 5e-3 \
+    --batch_size 16 \
+    --epochs 1000 \
+    --feat_type roi \
+    --optim adamw \
+    --warmup_ratio 0.05 \
+    --clip_grad_norm 5 \
+    --num_workers 8 \
+    --backbone 't5-base' \
+    --output $output \
+    --num_beams 5 \
+    --valid_batch_size 512 \
+    --load snap/pretrain/VLT5-novqa/Epoch30_base
+done
---
+++ snap/pretrain/VLT5-novqa/README.md
@@ -0,0 +1,5 @@
+# pretrained model weight
+
+- Download pretrained weight from [FewVLM](https://drive.google.com/file/d/17B-3TcXJ1tumNPYAQpg2MisSi9-7Dz-Y/view?usp=sharing) and saving them under the VLT5-novqa folder
+
+
